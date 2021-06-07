735a2f8e154e55f71bed3c85e978cd215536c06c
patriciamv97 <72247871+patriciamv97@users.noreply.github.com>
2021-06-07 17:58:51 +0200

    Ejercicio2  (#2)
    * Ejercicio1 terminado
    
    * Ejercicio2 terminado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,19 @@
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
 
     //Ejercicio 1
 
-     for (tabla7 in Ejercicio1(10).array){
+     /*for (tabla7 in Ejercicio1(10).array){
          println("$tabla7")
-     }
+     }*/
+
+    //Ejercicio 2
+
+    val arrayNumeros= Ejercicio2(10){ Random.nextInt(50,250) }
+    arrayNumeros.array.forEach { array ->
+        println("$array")
+    }
 
 
 }
